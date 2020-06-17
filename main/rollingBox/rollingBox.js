5d43fb0a853fddb918f75d906950c77512b3b4ef
sunhee5951 <sunhee5951@gmail.com>
2020-06-17 21:40:42 +0900

    first commit

---
+++ main/rollingBox/rollingBox.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+	/*------------------------전역변수 부---------------------- */
+	var  $rollingBox, $listBox, $listBox_ul, $listBox_ul_li
+	
+	$rollingBox = $('#rollingBox');
+	$listBox = $rollingBox.find('.inner>.listBox');
+	$listBox_ul = $listBox.children('ul');
+	$listBox_ul_li = $listBox_ul.children('li');
+	
+	$rollingBox = $('#rollingBox');
+	$listBox = $rollingBox.find('.listBox');
+	$listBox_ul = $listBox.children('ul');
+	$listBox_ul_li = $listBox_ul.children('li');
+	$rbtnPrev = $rollingBox.find(".rbtnPrev");
+	$rbtnNext = $rollingBox.find(".rbtnNext");
+	
+	var wid = $listBox_ul_li.outerWidth();
+	var num = $listBox_ul_li.length;
+	var totalWid = wid*num;
+	$listBox_ul.width(totalWid);
+	var mleft = 0;
+	
+	var timer = setInterval(move,20);
+	
+	/*------------------------이벤트 바인딩---------------------- */
+	$listBox.on("mouseenter",function(){
+		clearInterval(timer);
+	});
+	$listBox.on("mouseleave",function(){
+		timer = setInterval(move,20);
+	});
+	
+	$rbtnPrev.on("click",function(e){
+		e.preventDefault();
+		$listBox_ul_li.last().prependTo($listBox_ul);
+	});
+	
+	$rbtnNext.on("click",function(e){
+		e.preventDefault();
+		$listBox_ul_li.first().appendTo($listBox_ul);
+	});
+	
+	/*------------------------함수 정의 부---------------------- */
+	function move(){
+		mleft -=2;
+		if(mleft < -wid){
+			$(".list li").first().appendTo($listBox_ul);
+			mleft = 0;
+		}
+		$listBox_ul.css({"left":mleft});
+	}
+	
+	
+	
+	});
---
+++ main/visual/visual.js
@@ -0,0 +1,105 @@
+$(document).ready(function(){
+    /*------------------------전역변수 부---------------------- */
+    var $txtBox, $txtBox_ul, $txtBox_ul_li, $sliderBox, $sliderBox_ul, $sliderBox_ul_li, $floatBox, $floatBox_ul, $floatBox_ul_li, 
+    $btnNext,
+    
+    $txtBox = $('.txtBox');
+    $txtBox_ul = $txtBox.children('ul');
+    $txtBox_ul_li = $txtBox_ul.children('li');
+
+    $line = $('ul.line');
+    $line_ul_li = $line.children('li');
+
+    $sliderBox = $('.sliderBox');
+    $sliderBox_ul = $sliderBox.children('ul');
+    $sliderBox_ul_li = $sliderBox_ul.children('li');
+
+    $floatBox = $('.floatBox');
+    $floatBox_ul = $floatBox.children('ul');
+    $floatBox_ul_li = $floatBox_ul.children('li');
+
+    $visual = $('#visual');
+    $btnNext = $visual.find('.btnNext>li');
+
+    sliding({
+        type: 'text',
+        selector: '.txt1',
+        speed: 700,
+        delay: 0
+    }); 
+    sliding({
+        type: 'text',
+        selector: '.txt2',
+        speed: 700,
+        delay: 700
+    }); 
+    sliding({
+        type: 'text',
+        selector: '.txt3',
+        speed: 700,
+        delay: 1000
+    }); 
+    sliding({
+        type: 'box',
+        selector: '.box1',
+        speed: 600,
+        delay: 0
+    });
+
+    
+/*------------------------이벤트 바인딩---------------------- */
+    $btnNext.on('click' ,function(){
+		var i = $(this).index();
+        activation(i);
+    });
+    
+    /*------------------------함수 정의 부---------------------- */
+    function activation(i){
+        $txtBox_ul_li.removeClass('on');
+        $txtBox_ul_li.eq(i).addClass('on');
+        
+        $line_ul_li.removeClass('on');
+        $line_ul_li.eq(i).addClass('on');
+
+        $sliderBox_ul_li.removeClass('on');
+        $sliderBox_ul_li.eq(i).addClass('on');
+        
+        $floatBox_ul_li.removeClass('on');
+		$floatBox_ul_li.eq(i).addClass('on');
+
+		$btnNext.removeClass('on');
+		$btnNext.eq(i).addClass('on');
+    }
+
+    function sliding(options){   
+
+        var defaults = {
+            type: 'text',
+            selector: '.txt',
+            speed: 600,
+            delay:0 
+        }
+        var options = $.extend({},defaults,options);
+
+        if(options.type=='text'){
+            var bgColor = $(options.selector).children('span').css('color');
+        }else{
+            var bgColor = $(options.selector).attr('data-color');
+        }
+
+        $(options.selector).append(
+            $('<div class="mask">').css({
+                width:'100%', height:'100%', backgroundColor:bgColor, 
+                position:'absolute', top:0, left:'-100%'
+            })
+        );
+
+        $(options.selector).find('.mask').stop().delay(options.delay).animate({left:'0%'},options.speed,'easeInExpo',function(){
+            $(this).prev().css({opacity:1});
+            $(this).stop().animate({left:'100%'},options.speed, 'easeInExpo',function(){
+                $(this).remove();
+            })
+        })
+
+    }
+})
