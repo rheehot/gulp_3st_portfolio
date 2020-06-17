7eb926741f49f227075cfb3b41e31e641f0d6352
sunhee5951 <sunhee5951@gmail.com>
2020-06-18 00:36:54 +0900

    2commit

--- main/header/myScroll.js
+++ main/header/myScroll.js
@@ -1,89 +1,67 @@
-﻿(function($){
+﻿$(document).ready(function(){
 
-    $.defaults = {
-        btns : '#navi>li',
-        speed : 1000,
-        base : 0,
-        active : 'on'
-    }
+    var $boxs = $('section');
+    var $btns = $('#navi li');
+    var posArr = [];
+    var colors = [];
+    var speed = 1000;
+    var base = -300;
 
-    $.fn.myScroll = function(option){
-        option = $.extend(null,$.defaults, option);
-        new MyScroll(this, option);
-        return this;
-    }
-
-    function MyScroll(selector, option){
-        var self = this;
-        self.option = option;
-        self.init(selector);
-        //처음 로딩시 동적으로 만들어지는 DOM의 시간을 벌어주기 위해 1초뒤에 섹션별 offset.top값을 저장
-        setTimeout(function(){
-            self.setPos();
-        },1000);
-        //this.setPos();
-        self.bindingEvent();
-    }    
-
-    MyScroll.prototype.init = function(selector){
-        this.$body = $('html,body');
-        this.$boxs = $(selector);             
-        this.$btns = $(this.option.btns);
-        this.len = this.$btns.length;
-        this.speed = this.option.speed;
-        this.base = this.option.base;
-        this.active = this.option.active;
-        this.pos = [];
-    }
-
-    MyScroll.prototype.setPos = function(){
-        var self = this;
-        self.pos = [];
-        
-        //소수점으로 쪼개질 경우 정수로 변환
-        $(self.$boxs).each(function(){
-            self.pos.push( parseInt($(this).offset().top));           
-        }); 
-        self.pos.push(parseInt(self.$boxs.last().offset().top + self.$boxs.last().height()));
-        //console.log(self.pos);
+    $boxs.each(function(){
+        posArr.push( $(this).offset().top );
+    });
+    //console.log(colors);
     
-    }  
+    posArr.push( $boxs.last().offset().top + $boxs.last().outerHeight() );
 
-    MyScroll.prototype.bindingEvent = function(){
-        var self = this;
+    $(window).on('scroll',function(){
+        var scroll  = $(this).scrollTop();
 
-        $(window).on('resize',function(){
-            self.setPos();
-        });
+        $btns.removeClass('on');
+        $boxs.removeClass('on');
 
-        $(window).on('scroll',function(){
-            var scroll = $(window).scrollTop();
-            self.activation(scroll);
-        });
-
-        self.$btns.on('click',function(e){
-            e.preventDefault();
-            var target = $(this).children('a').attr('href');
-            self.moveScroll(target);
-        })
-    }
-
-    MyScroll.prototype.activation = function(scroll){
-        this.$btns.removeClass(this.active);
-        this.$boxs.removeClass(this.active);
-
-        for(var i=0; i<this.len; i++){
-            if(scroll>=this.pos[i]+this.base && scroll<this.pos[i+1]+this.base){
-                this.$btns.eq(i).addClass(this.active);
-                this.$boxs.eq(i).addClass(this.active);
+        for(var i=0; i<posArr.length; i++){
+            if(scroll>=posArr[i]-300 && scroll<posArr[i+1]-300){
+                $btns.eq(i).addClass('on');
+                $boxs.eq(i).addClass('on');
             }
         }
-    }
+    });
 
-    MyScroll.prototype.moveScroll = function(target){
-        var target_pos = parseInt($(target).offset().top);
-        console.log(target_pos);
-        this.$body.stop().animate({scrollTop : target_pos},this.speed);
-    }
+    $btns.on('click',function(e){
+        e.preventDefault();
+        var target = $(this).children('a').attr('href');
+        var target_pos = $(target).offset().top;
 
-})(jQuery);
+        $('html,body').stop().animate({scrollTop : target_pos}, speed);
+    });
+
+    $boxs.on('mousewheel',function(event, delta){
+        event.preventDefault();
+
+        var i = $(this).index();
+
+        if(delta>0){
+            if(i !== 0 ){
+                var prev_pos = $(this).prev().offset().top;
+                $('html,body').stop().animate({scrollTop : prev_pos},speed);
+            }
+        }
+
+        if(delta<0){
+            if(i !== $boxs.length-1 ){
+                var next_pos = $(this).next().offset().top;
+                $('html,body').stop().animate({scrollTop : next_pos},speed);
+            }            
+        }
+    })
+    
+
+});
+
+
+
+
+
+
+
